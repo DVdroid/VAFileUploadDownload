ee8c5304142e3e2590476ba9a36a97b82663c883
VA <anandin02@gmail.com>
2022-04-25 08:23:50 +0530

    Initial Commit

---
+++ VAFileUploadDownload/VAFileUploadDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  VAFileUploadDownloadApp.swift
+//  VAFileUploadDownload
+//
+//  Created by Vikash Anand on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct VAFileUploadDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
